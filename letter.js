02af510f985952153ca115f264b98d8e40723ef3
masonposch <masonposch@Mason-Poschs-MacBook-Pro.local>
2016-10-19 23:44:02 -0500

    adding user letter checking to word.js

--- letter.js
+++ letter.js
@@ -3,6 +3,49 @@
 //whether or not a letter appears as a "_" or as itself on-screen.
 
 
+//STORES THE RANDOM WORD
+var word = require('./game.js');
+
+var gameWord = word.randomWord();
+
+console.log(gameWord);
+
+//Turns random word into an array
+var gameWord = gameWord.split('');
+
+console.log(gameWord);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Runs the function from game.js that chooses the random word
 // function letterGuess(word){
 // 	this.correct = function(){
@@ -12,14 +55,3 @@
 // 		}
 // 	}
 // }
-
-
-//STORES THE RANDOM WORD
-var word = require('./game.js');
-
-var test = word.randomWord();
-
-//Turns random word into an array
-var fda = test.split('');
-
-console.log(fda);
